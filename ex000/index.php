2d92907dfeedc18c78a9d2314cedbb68255d9681
Fox Troll <ruydomingos3@gmail.com>
2024-06-10 16:03:16 -0300

    Primeiras aulas

---
+++ ex000/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Me livrando da Maldição</title>
+</head>
+
+<body>
+    <h1>
+        <?php
+        print "Olá, Mundo! 🌍";
+        ?>
+        <p>
+            Vamos tentar nos livrar da maldição
+        </p>
+    </h1>
+</body>
+
+</html>
---
+++ ex002/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+
+<body>
+
+</body>
+<h1>
+    Exemplo de php
+</h1>
+
+<?php
+date_default_timezone_set("America/Sao_Paulo");
+print "Hoje é dia" . date("d/M/Y ");
+print "e a hora atual é " . date("G:i:s T");
+?>
+
+</html>
