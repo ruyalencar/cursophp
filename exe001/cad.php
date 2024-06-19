aa3a5e37ae92362c8786e223e3688ba65d05d152
Fox Troll <ruydomingos3@gmail.com>
2024-06-19 17:31:39 -0300

    aulas 27

---
+++ exe001/cad.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resoltado do desafio</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <h1>Resultado Final</h1>
+    </header>
+    <main>
+
+        <?php
+        $numero = $_GET["numero"];
+        $r1 = $numero + 1;
+        $r2 = $numero - 1;
+        print "<p>O numero escolhido foi <strong>$numero</strong></p>";
+        print "<p>O seu antecessor é $r2</p>";
+        print "<p>O seu sucessor é $r1</p>";
+        ?>
+        <input type="submit" value="Voltar">
+    </main>
+</body>
+
+</html>
---
+++ exe002/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio 2</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <section>
+        <?php
+        print "<p>Trabalhando com números aleatórios</p>";
+        print "<p>Gerando um número aleatório entre 0 e 100...</p>";
+        $n = rand(0, 100);
+        print "<p>o valor gerado foi $n</p>";
+        ?>
+    </section>
+
+</body>
+
+</html>
